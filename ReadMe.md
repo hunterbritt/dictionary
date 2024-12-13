0968cc4ca4535fd0dc9400cae15e79b57acd0ab3
Hunter Britt <devxprogramming@gmail.com>
2024-12-13 18:30:48 +0000

    Added more code :)

---
+++ ReadMe.md
@@ -0,0 +1,27 @@
+# Dev Dictionary
+A simple dictionary app that allows users to search for the meaning of words. This app uses the [Dictionary API](https://www.dictionaryapi.dev/) to fetch the data.
+
+## Features
+- Search for the meaning of words
+- Pronunciation of words
+- Examples of words
+- Synonyms of words
+- Antonyms of words
+- Sources of words
+
+## Issues.
+I must say, I'm not a fan of the design of the app. The UI is a bit cluttered and the layout but it's a good start.
+
+## Screenshots
+
+
+## How to Contribute
+1. Fork the repository
+2. Clone the repository
+3. Create a new branch
+4. Make your changes
+5. Push your changes
+6. Make a pull request
+
+## License
+Comming Soon.
--- script.js
+++ script.js
@@ -8,6 +8,12 @@ const example = document.getElementById('example');
 const synonyms = document.getElementById('synonyms');
 
 const searchWord = async (word) => {
+    if (!window.ononline) {
+        document.querySelector('.info').innerHTML = "No internet connection";
+    }
+    else {
+        document.querySelector('.card').style.display = 'none';
+    }
     const response = await fetch(API_URL + word);
     const data = await response.json();
     console.log(data);
@@ -17,30 +23,29 @@ const searchWord = async (word) => {
 
 
 searchBtn.addEventListener("click", async () => {
-    try{
-        if (search.value === '') {
-            document.querySelector('.info').style.display = 'block';
-            document.querySelector('.info').innerHTML = "Please enter a word";
-            setTimeout(() => {
-                document.querySelector('.info').style.display = 'none';
-            }, 3000);
-        } else {
+    try {
+        if (!search.value === '' || searchWord.status === 404) {
             const response_data = await searchWord(search.value)
             if (!response_data || response_data.length === 0) {
                 alert("Word not found");
             }
             document.querySelector('.card').style.display = 'block';
-            document.querySelector('.word').innerHTML = `Word: ${response_data[0].word}`;
+            document.querySelector('.word').innerHTML = `Word: ${search.value}`;
             if (response_data[0].meanings.length > 1) {
                 response_data[0].meanings.forEach((meaning) => {
-                    document.querySelector('.definition').innerHTML = `Definition (${meaning.partOfSpeech}): ${meaning.definitions[0].definition}`;
+                    document.querySelector('.definition').innerHTML = `(${meaning.partOfSpeech}): ${meaning.definitions[0].definition}`;
                 })
-            } else{
-                
-                document.querySelector('.definition').innerHTML = `Definition: ${response_data[0].meanings[0].definitions[0].definition}`;
+            } else if (response_data[0].meanings.length === 1) {
+
+                document.querySelector('.definition').innerHTML = `${response_data[0].meanings[0].definitions[0].definition}`;
+            } else {
+                document.querySelector('.info').innerHTML = `${response_data.title} - ${response_data.message}`
             }
+            document.querySelector('.definition').classList.add('highlight');
+            document.querySelector('.source').classList.add('highlight');
+
             search.value = "";
-            if(response_data[0].meanings[0].antonyms.length > 0 || response_data[0].meanings[0].synonyms.length > 0){
+            if (response_data[0].meanings[0].antonyms.length > 0 || response_data[0].meanings[0].synonyms.length > 0) {
 
                 document.querySelector('.synonyms').innerHTML = `Synonyms: [${response_data[0].meanings[0].synonyms},]`;
                 document.querySelector('.antonyms').innerHTML = `Antonyms: [${response_data[0].meanings[0].antonyms},]`;
@@ -50,9 +55,52 @@ searchBtn.addEventListener("click", async () => {
             }
             document.querySelector('.source').innerHTML = `${response_data[0].sourceUrls}`
             document.querySelector('.source').href = response_data[0].sourceUrls[0];
+        } else {
+            document.querySelector('.info').style.display = 'block';
+            document.querySelector('.info').innerHTML = "Please enter a word";
+            setTimeout(() => {
+                document.querySelector('.info').style.display = 'none';
+            }, 3000);
         }
-        
-    }catch(error){
-        console.log(error);
+
+    } catch (error) {
+        alert(`Error: ${error}`);
+    }
+})
+
+
+search.addEventListener('keydown', async (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        if (search.value !== '') {
+            const response_data = await searchWord(search.value);
+            if (!response_data || response_data.length === 0) {
+                alert("Word not found");
+            }
+            document.querySelector('.card').style.display = 'block';
+            document.querySelector('.word').innerHTML = `Word: ${search.value}`;
+            if (response_data[0].meanings.length > 1) {
+                response_data[0].meanings.forEach((meaning) => {
+                    document.querySelector('.definition').innerHTML = `(${meaning.partOfSpeech}): ${meaning.definitions[0].definition}`;
+                })
+            } else if (response_data[0].meanings.length > 1) {
+                document.querySelector('.definition').innerHTML = `${response_data[0].meanings[0].definitions[0].definition}`;
+            } else {
+                document.querySelector('.info').innerHTML = `${response_data.title} - ${response_data.message}`
+            }
+            document.querySelector('.definition').classList.add('highlight');
+            document.querySelector('.source').classList.add('highlight');
+
+            search.value = "";
+            if (response_data[0].meanings[0].antonyms.length > 0 || response_data[0].meanings[0].synonyms.length > 0) {
+                document.querySelector('.synonyms').innerHTML = `Synonyms: [${response_data[0].meanings[0].synonyms},]`;
+                document.querySelector('.antonyms').innerHTML = `Antonyms: [${response_data[0].meanings[0].antonyms},]`;
+            } else {
+                document.querySelector('.synonyms').innerHTML = `No Synonyms`;
+                document.querySelector('.antonyms').innerHTML = `No Antonyms`;
+            }
+            document.querySelector('.source').innerHTML = `${response_data[0].sourceUrls}`;
+            document.querySelector('.source').href = response_data[0].sourceUrls[0];
+        }
     }
 })
